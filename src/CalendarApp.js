0d82129aa0be724f34de448b8843b2acc63d31c8
Sebastian Asis <sasisromero10@gmail.com>
2020-12-16 10:18:59 -0500

    react router, react big calendar and moment

---
+++ src/CalendarApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppRouter } from './router/AppRouter'
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <AppRouter/>
+        </div>
+    )
+}
---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from '../ui/Navbar'
+
+export const CalendarScreen = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
